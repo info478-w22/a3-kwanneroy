dd5e5533b47aee1160ca27bd9b3fd734028a1ea6
katw15 <katw15@uw.edu>
2022-03-10 00:36:08 -0800

    Completed assignment

--- app_server.R
+++ app_server.R
@@ -1,6 +1,26 @@
+library("shiny")
 library("tidyverse")
 library("dplyr")
 
 server <- function(input, output) {
-
+  require(EpiModel)
+  
+  #Deterministic Model 
+  param <- param.dcm(inf.prob = 0.5, act.rate = c(2))
+  init <- init.dcm(s.num = 999, i.num = 1)
+  control <- control.dcm(type = "SI", nsteps = 365)
+  
+  mod <- dcm(param, init, control)
+  mod
+  plot(mod)
+  summary(mod, at=100)
+  
+  comp_plot(mod, at = 2, digits = 1)
+  
+  #Stochastic Model 
+  param <- param.icm(inf.prob = 0.2, act.rate = 0.25)
+  init <- init.icm(s.num = 500, i.num = 1)
+  control <- control.icm(type = "SI", nsims = 10, nsteps = 300)
+  mod <- icm(param, init, control)
+  return(mod)
 }
--- app_ui.R
+++ app_ui.R
@@ -6,18 +6,34 @@ library(shinythemes)
 page_one <- tabPanel(
   "Introduction",
   mainPanel(   
-    h1(""),
-    p("text")
+    h1("Overview and Intention"),
+    p("This is an interactive disease modeling simulation of COVID-19 spread.")
   ))
 # Widgets 
 
 # Page 2
 page_two <- tabPanel(
-  "",
-  h1(""),
-  p("text"),
-  sidebarLayout(             
-    sidebarPanel(
-      h2(""),
-      p(""))
-))
+  "Deterministic Model",
+  p("text")
+)
+
+# Page 3
+page_three <- tabPanel(
+  "Stochastic Model",
+  p("text")
+)
+
+# Page 4
+
+page_four <- tabPanel(
+  "Interpretation",
+  p("Observations")
+)
+
+ui <- navbarPage(theme = shinytheme("journal"),
+                 "Assignment 3: Disease Modeling",
+                 page_one,
+                 page_two,
+                 page_three,
+                 page_four
+)
